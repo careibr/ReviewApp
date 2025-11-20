3f1b0feb01cc076545b91217e603777a4e4f9d30
careibr <careib@ensworth.com>
2025-11-20 11:45:44 -0600

    Day 1

--- src/QuestionBank.java
+++ src/QuestionBank.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
 public class QuestionBank {
+    private ArrayList<Questions> questions;
+
+    public QuestionBank()
+    {
+        questions = new ArrayList<Questions>();
+        loadQuestions();
+    }
+
+    private void loadQuestions()
+    {
+
+        questions.add(new Questions("What is the first element in a Java array?",
+                new String[]{"0","1","-1","It depends"},0,"arrays",
+                "Java arrays are zero-based, so the first index is 0"));
+
+    }
+
+    public ArrayList<Questions> getQuestionByTopic(String topic)
+    {
+        ArrayList<Questions> result = new ArrayList<Questions>();
+        for(Questions q : questions)
+        {
+            if(topic.equals("all") || )
+        }
+
+    }
+
+
+
 }
